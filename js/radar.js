b36a983aa44c533d677ceb5fc026836472f4b1db
root <root@ip-172-31-27-97.eu-west-1.compute.internal>
2016-02-03 15:01:35 +0000

    multiple visual changes to interface

--- js/radar.js
+++ js/radar.js
@@ -11,24 +11,59 @@ var greenIcon = L.icon({
     iconAnchor: [0, 0],
     popupAnchor: [0, 0]
 });
+
+var achistory = {};
+var acvisible = [];
+var polylines = {};
+
 var map = L.map('map').setView([52.483, -1.89], 7),
 
 realtime = L.realtime({
-    url: 'http://nucleardawn.ddns.net/radar2/feeders/geojson_aircraft_vrs.php',
+	url: 'feeders/geojson_aircraft_vrs.php?milOnly=true',
     crossOrigin: true,
     type: 'json'
 }, {
-    interval: 5 * 1000,
+    interval: 3 * 1000,
     pointToLayer: function (feature, latlng) {
+		acvisible.push(feature.properties.id);
+		if (typeof(achistory[feature.properties.id]) == "undefined") {
+			achistory[feature.properties.id] = [];
+		} else {
+			achistory[feature.properties.id].push(latlng);
+		}
 		if (feature.properties.callsign == null) {
 			feature.properties.callsign = '';
 		}
-
-        return L.marker(latlng, {icon: greenIcon}).bindLabel(feature.properties.callsign +
-           '<br/>' + Math.round(feature.properties.heading)+
-           '<br/>' + Math.round(feature.properties.speed), { noHide:true, className: "aircraftLabel", fillOpacity: 0, opacity: 0.6, weight: 1  }).addTo(map);
+		var shortAlt = Math.round(feature.properties.altitude / 100);
+		
+		return L.marker(latlng, {icon: greenIcon}).bindLabel(feature.properties.callsign +
+			'<br/>' + feature.properties.model +
+			'<br/>FL' + shortAlt + 
+			' - ' + Math.round(feature.properties.heading) +
+			'<br/>' + feature.properties.reg, { noHide:true, className: "aircraftLabel", fillOpacity: 0, opacity: 0.6, weight: 1  }).addTo(map);
     }                            
 }).addTo(map);  
+
+realtime.on('update', function(data) {
+	for(var id in achistory) {
+		if (achistory.hasOwnProperty(id)) {
+			if (acvisible.indexOf(id)) {
+				//console.log(achistory[id]);
+				if (achistory[id].length == 1) {
+					var polyline = L.polyline(achistory[id], {className: "aircraftLabel", dashArray: "5,5", fillOpacity: 0, opacity: 0.6, weight: 3, color: "green" }).addTo(map);
+					polylines[id] = polyline;
+				} 
+				if (achistory[id].length > 1) {
+					polylines[id].setLatLngs(achistory[id]);
+				}
+				achistory[id] = achistory[id].slice(-10);
+			} else {
+				delete achistory.id;
+				delete polylines[id];
+			}
+		}
+	}
+});
 
 // Distance Markers from home location
 L.circle([52.395400,-4.070829], 10000, { color: "#40FF5A", fillOpacity: 0, opacity: 0.2, weight: 2 }).addTo(map);
@@ -44,6 +79,7 @@ L.tileLayer('http://{s}.basemaps.cartocdn.com/dark_nolabels/{z}/{x}/{y}.png', {
 	maxZoom: 19
 }).addTo(map);
 
+/*
 var geojsonReportingPointsLayer = new L.GeoJSON.AJAX("json/reporting_points.json",{
 	middleware:function(data){
 	   return L.geoJson(data, {
@@ -54,8 +90,8 @@ var geojsonReportingPointsLayer = new L.GeoJSON.AJAX("json/reporting_points.json
 	    }).addTo(map);
 	}
 });
-
-var geojsonAirspaceLayer = new L.GeoJSON.AJAX("json/airspace_simple.json",{
+*/
+var geojsonAirspaceLayer = new L.GeoJSON.AJAX("json/airspace.json",{
 	middleware:function(data){
 	   return L.geoJson(data, {
 			pointToLayer: function (feature, latlng) {
@@ -69,6 +105,10 @@ var geojsonAirspaceLayer = new L.GeoJSON.AJAX("json/airspace_simple.json",{
 					case 'PROHIBITED': return { color: "#ff0000", fillOpacity: 0.2, opacity: 0.2, weight: 2 };
 					case 'DANGER': return { color: "#ff0000", fillOpacity: 0.2, opacity: 0.2, weight: 2 };
 					case 'RESTRICTED': return { color: "#ff8000", fillOpacity: 0.2, opacity: 0.2, weight: 2 };
+					case 'MATZ': return { color: "#0000ff", fillOpacity: 0.2, opacity: 0.2, weight: 2 };
+					case 'GSEC': return { color: "#ffff00", fillOpacity: 0, opacity: 0, weight: 1 };
+					case 'CTA\/CTR': return { color: "#ffffff", fillOpacity: 0, opacity: 0.1, weight: 1 };
+					case 'AIRWAYS': return { color: "#0000ff", fillOpacity: 0.1, opacity: 0.0, weight: 1 };
 					default: return { color: "#ffffff", fillOpacity: 0, opacity: 0.1, weight: 2 };
 				}
 		    },
